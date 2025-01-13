a68e6dc529c4e3254aee09d16bf0a90e22682715
Tomas Franco Gimenes <eltomyprog@gmail.com>
2025-01-13 03:21:27 -0300

    se creo usuario admin para swagger

--- src/main/java/com/heavydelay/config/SecurityConfig.java
+++ src/main/java/com/heavydelay/config/SecurityConfig.java
@@ -2,8 +2,15 @@ package com.heavydelay.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -11,5 +18,27 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/sys/api/user/register", "/sys/api/user/login").permitAll()
+                .anyRequest().authenticated()
+            )
+            .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder){
+        UserDetails admin = User.withUsername("admin")
+            .password(passwordEncoder.encode("112233"))
+            .roles("ADMIN")
+            .build();
+        
+            return new InMemoryUserDetailsManager(admin);
     }
 }
